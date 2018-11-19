856d934aff5e72ea676b5e5295036d376d386340
PankratovIA <PankratovIA.mechanic@gmail.com>
2018-11-19 22:53:28 +0400

    Elliptic (5.4.6) works only for for Nx = Ny

--- SteadyFlow/FDM.py
+++ SteadyFlow/FDM.py
@@ -1,10 +1,11 @@
 import numpy as np
+np.set_printoptions(precision=3, suppress=True)
 
 WIDTH = 1.0
 HEIGTH = 1.0
 
-Nx = 3
-Ny = 3
+Nx = 5
+Ny = 5
 SIZE = Nx * Ny
        
 
@@ -15,7 +16,10 @@ eps = 1e-9
 
 phi = np.zeros((Ny+2, Nx+2))
 
-def cellNum(row, col):
+phi = np.random.rand(Ny+2, Nx+2)
+
+def cellNum(t):
+    row, col = t
     assert 1<=row<=Ny
     assert 1<=col<=Nx
     return (row - 1) * Ny + col - 1
@@ -28,34 +32,55 @@ def createMatrix(Nx, Ny):
     for row in range(1, Ny):
         for col in range(1, Nx):
             if not onBoundary(row, col):
-                num = cellNum(row, col)
-                A[num][num] = 10
+                num = cellNum((row, col))
+                #A[num][num] = 10
+                
+                phi_xc = (phi[row][col+1] - phi[row][col-1]) / (2 * dx)
+                print("phi_xc =", phi_xc)
+                
+                phi_xb = (phi[row][col] - phi[row][col-2]) / (2 * dx)
+                print("phi_xb =", phi_xb)
+                
+                if (phi_xb < 0) & (phi_xc < 0):
+                    # elliptic (5.4.6)
+                    print(row, col, "elliptic (5.4.6)")
+                    first = map(cellNum, [(row, col+1), (row, col-1)])
+                    for cur in first:
+                        A[num][cur] += phi_xc / (dx * dx)
+                    A[num][num] -= 2 * phi_xc / (dx * dx)
+                    
+                    second = map(cellNum, [(row+1, col), (row-1, col)])
+                    for cur in second:
+                        A[num][cur] -= 1.0 / (dy * dy)
+                    A[num][num] += 2.0 / (dy * dy)
+                else:
+                    print("Error")
     return A
 
 def createBC(A):
     b = np.zeros((SIZE, 1))
     for col in range(1, Nx+1):
         # phi(x, 0) = 0
-        num = cellNum(1, col)
+        num = cellNum((1, col))
         A[num] = np.zeros((1, SIZE))
         A[num][num] = 1.0
         b[num] = 0.0
             
         # phi(x, b) = 0
-        num = cellNum(Nx, col)
+        num = cellNum((Nx, col))
         A[num] = np.zeros((1, SIZE))
         A[num][num] = 1.0
         b[num] = 0.0
 
     for row in range(1, Ny+1):
         # phi(0, y) = 1
-        num = cellNum(row, 1)
+        num = cellNum((row, 1))
         A[num] = np.zeros((1, SIZE))
         A[num][num] = 1.0
         b[num] = 1.0
            
         # phi(a, y) = 0
-        num = cellNum(row, Nx)
+        num = cellNum((row, Nx))
         A[num] = np.zeros((1, SIZE))
         A[num][num] = 1.0
         b[num] = 0.0
@@ -73,6 +98,18 @@ if __name__ == "__main__":
     for i in range(Ny+2):
         phi[i][0] = 1.0
         phi[i][1] = 1.0
+        
+        phi[i][Nx] = 0.0
+        phi[i][Nx+1] = 0.0
+        
+        
+    for i in range(2, Nx+2):
+        phi[0][i] = 0.0
+        phi[1][i] = 0.0
+        
+        phi[Ny][i] = 0.0
+        phi[Ny+1][i] = 0.0
+        
     print("phi = \n{0}".format(phi))
     print("BC <<<")
     
@@ -96,11 +133,12 @@ if __name__ == "__main__":
         mx = 0
         for row in range(1, Ny+1):
             for col in range(1, Nx+1):
-                num = cellNum(row, col)
+                num = cellNum((row, col))
                 diff = abs(phi[row][col] - x[num])
                 mx = max(mx, diff)
                 phi[row][col] = x[num]
         print("mx = {0}".format(mx, "%.3f"))
         if mx < eps:
             break
+    print("iter = ", iter)
     print("Result phi = \n{0}".format(phi))
