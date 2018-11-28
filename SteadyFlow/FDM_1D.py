1af34edaeb7f4a25728e05edd1ec8a8105b92694
PankratovIA <PankratovIA.mechanic@gmail.com>
2018-11-28 15:48:58 +0400

    bad switching, no convergence

--- SteadyFlow/FDM_1D.py
+++ SteadyFlow/FDM_1D.py
@@ -24,8 +24,10 @@ dPhi_0 = np.tan(alpha)
 
 Phi_a = 0.8
 
-Nx = 11
+Nx = 51
 dx = WIDTH / (Nx - 1)
+
+eps = 1e-9
 
 def analytical_solution(x):
     xu = Phi_a - Phi_0 - K * WIDTH / (gamma + 1)
@@ -47,6 +49,39 @@ def parabola(x):
     A_ = (Phi_a - Phi_0 - dPhi_0*WIDTH) / (WIDTH ** 2.0)
     return A_ * x * x + dPhi_0 * x + Phi_0
 
+def createMatrix(phi):
+    A = np.zeros((Nx, Nx))
+    b = np.zeros((Nx, 1))
+    # phi(0) = Phi_0
+    A[0][0] = 1.0
+    b[0] = Phi_0
+    
+    # dphi(0) = dPhi_0
+    A[1][1] = 1.0 / dx
+    A[1][0] = -1.0 / dx
+    b[1] = dPhi_0
+    
+    for row in range(2, Nx-1):
+        dphi = (phi[row] - phi[row - 1]) / dx
+        if dphi < dPhi_0:
+            # K - (gamma + 1) * phi_x = 0
+            #print("phi_x")
+            A[row][row] = -1.0 / dx
+            A[row][row+1] = 1.0 / dx
+            b[row] = K / (gamma + 1)
+        else:
+            # phi_xx = 0
+            #print("phi_xx")
+            A[row][row-1] = 1.0 / (dx * dx)
+            A[row][row] = -2.0 / (dx * dx)
+            A[row][row+1] = 1.0 / (dx * dx)
+            # b[row] = 0
+
+    A[Nx-1][Nx-1] = 1.0
+    b[Nx-1] = Phi_a
+    return A, b
+
+
 if __name__ == "__main__":
     print("FDM 1D")
     
@@ -61,5 +96,30 @@ if __name__ == "__main__":
     
     print("x =", x)
     print("phi =", phi)
+    
+    print("dphi = ", [(x[1] - x[0]) / dx for x in zip(phi, phi[1:])])
+    
+    cnt = 0
+    while cnt < 200:
+        cnt += 1
+        print("Iteration {0}".format(cnt))
+        A, b = createMatrix(phi)
+        #print("A =\n", A)
+        #print("b =\n", b)
+    
+        cur = np.linalg.solve(A, b)
+        #print("cur = \n{0}".format(cur))
+        
+        mx = 0
+        for idx in range(Nx):
+            diff = abs(phi[idx] - cur[idx])
+            mx = max(mx, diff)
+            phi[idx] = cur[idx]
+        print("mx = {0}".format(mx, "%.3f"))
+        if mx < eps:
+            break
+    print("iter = ", cnt)
+    print("Result phi = \n{0}".format(phi))
+
     
     
--- SteadyFlow/FDM_1D_Graph.py
+++ SteadyFlow/FDM_1D_Graph.py
@@ -4,17 +4,25 @@ import matplotlib.pyplot as plt
 if __name__ == "__main__":
     print("FDM 1D Graph")
     
-    N = 1001
+    N = 101
     x = np.linspace(0, WIDTH, N)
     print("x = ", x)
     y = np.array(list(map(analytical_solution, x)))
     par = np.array(list(map(parabola, x)))
     plt.plot(x, y)
-    plt.plot(x, par)
+    plt.plot(x, par, '--')
+    
+    phi = [ 0.250,  0.300,  0.375,  0.450,  0.525,  0.600,  0.675,  0.749,  0.824,  0.899,
+  0.890,  0.881,  0.872,  0.863,  0.854,  0.845,  0.836,  0.827,  0.818,  0.809, 0.800]
+      
+    x = np.linspace(0, WIDTH, len(phi))
+    print("x = ", x)
+    plt.plot(x, phi, 'o')
+    
     plt.ylim()
     print(plt.ylim())
     bottom, top = plt.ylim()  # return the current ylim
-    #plt.ylim((0.95*bottom, 1.05*top))   # set the ylim to bottom, top
+    plt.ylim((0.95*bottom, 1.05*top))   # set the ylim to bottom, top
     plt.show()
     
     
